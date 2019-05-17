55f9c744d3bdf532ad1ac0eea6b6925492438cfb
davidbecker6081 <dbecker@peaksware.com>
2019-05-17 09:59:17 -0600

    Finish linkedList with prepend and nth functions

--- Eloquent-Javascript/chapter4/linkedList.js
+++ Eloquent-Javascript/chapter4/linkedList.js
@@ -25,9 +25,53 @@ const arrayToList = (arr) => {
 
 console.log(arrayToList([1,2,3]))
 
+// -> {
+    // value: 1,
+    // rest: {
+    //     value: 2,
+    //     rest: {
+    //         value: 3,
+    //         rest: null
+    //     }
+    // }
 // Also write a listToArray function that produces an array from a list. Then add a helper function prepend,
 // which takes an element and a list and creates a new list that adds the element to the front of the input list,
+const listToArray = (list) => {
+    const arr = []
+    for (let node = list; node; node = node.rest) {
+        arr.push(node.value)
+    }
+    return arr
+}
+
+console.log(listToArray(arrayToList([1,2,3])))
+
+//console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+
+const prepend = (el, list) => {
+    const arr = listToArray(list)
+    arr.unshift(el)
+
+    return arrayToList(arr)
+}
+
+console.log(prepend(10, prepend(20, null)))
+
 // and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element)
 // or undefined when there is no such element.
+
+// To get element N + 1, you take the Nth element of the list that’s in this list’s rest property.
+
+//console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
+
+const nth = (list, num) => {
+    for (let node = list, i = 0; node; node = node.rest, i++) {
+        if (i === num) {
+            return node.value
+        }
+    }
+}
 
 // If you haven’t already, also write a recursive version of nth.
